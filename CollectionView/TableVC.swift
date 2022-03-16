7d400db41e2833e12304342c2a1f30aa34b7a1aa
Muhammad Hassan <MuhammadHassan518@gmail.com>
2022-03-16 19:51:33 +0500

    .

--- CollectionView/TableVC.swift
+++ CollectionView/TableVC.swift
@@ -8,21 +8,22 @@
 import UIKit
 
 class TableVC: UITableViewController {
-    
-    
-    
+
+
+
     let categrayname = ["Action","Drama","Movies","Music","Jokes"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
         }
 
 
     override func numberOfSections(in tableView: UITableView) -> Int { 1 }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categrayname.count }
-    
+        return categrayname.count
+
+    }
+
    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! cell
         cell.lblName.text = categrayname[indexPath.row]
